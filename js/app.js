9e184bb0bb752344eb3da3345d14cb4f718cb2ed
Roy A Vanegas <roy@thecodeeducators.com>
2019-04-24 13:40:18 -0400

    Update code to show example of various ways of declaring and initializing variables

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 window.onload = () => {
-    alert("Hello, world");
+    let x = (10 * 10) + 20;
+    let y = 1;
+    let sum = (x + y);
+    let firstName = `Ed`;
+    let lastName = `Bob`;
+    const UNIVERSITY = `NYU`;
+
+    lastName = `Norton`;
+
+    console.log(`Hey, x + y is ${sum}`);
+    console.log(typeof sum);
+    console.log(`${firstName} ${lastName}`);
+    console.log(typeof name);
+    console.log(`${firstName} ${lastName} does not attend ${UNIVERSITY}`)
 };
