f98b259753ff03fd2c27d11381091ba57dd26928
Roy A Vanegas <roy@thecodeeducators.com>
2019-04-24 14:01:37 -0400

    Remove JavaScript from previous example

--- js/app.js
+++ js/app.js
@@ -1,16 +1,3 @@
 window.onload = () => {
-    let x = (10 * 10) + 20;
-    let y = 1;
-    let sum = (x + y);
-    let firstName = `Ed`;
-    let lastName = `Bob`;
-    const UNIVERSITY = `NYU`;
 
-    lastName = `Norton`;
-
-    console.log(`Hey, x + y is ${sum}`);
-    console.log(typeof sum);
-    console.log(`${firstName} ${lastName}`);
-    console.log(typeof name);
-    console.log(`${firstName} ${lastName} does not attend ${UNIVERSITY}`)
 };
